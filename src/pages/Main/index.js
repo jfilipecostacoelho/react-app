90b72297886dc4691b13a6d9febcab7f63ba7c81
jfilipecostacoelho <jfilipecostacoelho@gmail.com>
2025-02-13 15:27:52 +0000

    Create button animation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useCallback} from 'react';
-import {FaGithub, FaPlus} from 'react-icons/fa';
+import {FaGithub, FaPlus, FaSpinner} from 'react-icons/fa';
 import {Container, Form, SubmitButton} from "./styles";
 
 import api from '../../services/api';
@@ -8,19 +8,27 @@ export default function Main(){
 
     const [newRepo, setNewRepo] = useState('');
     const [repositorios, setRepositorios] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = useCallback((e)=>{
         e.preventDefault();
         
         async function submit(){
-            const response = await api.get(`repos/${newRepo}`);
+            setLoading(true);
+            try{
+                const response = await api.get(`repos/${newRepo}`);
 
-            const data = {
-                name: response.data.full_name,
+                const data = {
+                    name: response.data.full_name,
+                }
+
+                setRepositorios([...repositorios, data]);
+                setNewRepo('');
+            }catch(error){
+                console.log(error);
+            }finally{
+                setLoading(false);
             }
-
-            setRepositorios([...repositorios, data]);
-            setNewRepo('');
         }
 
         submit();
@@ -46,8 +54,12 @@ export default function Main(){
                 onChange={handleinputChange}
                 />
 
-                <SubmitButton>
-                    <FaPlus color="#FFF" size={14}/>
+                <SubmitButton loading={loading ? 1 : 0}>
+                    {loading ? (
+                        <FaSpinner color="#FFF" size={14}/>
+                    ) : (
+                        <FaPlus color="#FFF"size={14}/>
+                    )}
                 </SubmitButton>
 
             </Form>
