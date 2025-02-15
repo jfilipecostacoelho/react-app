62868d48c4a9248d4342fa4c9d203d7f12756fde
jfilipecostacoelho <jfilipecostacoelho@gmail.com>
2025-02-15 16:33:22 +0000

    Styled issues data and added pagination

--- src/pages/Repos/index.js
+++ src/pages/Repos/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
-import {Loading, Container, Owner} from './styles';
+import {Loading, Container, Owner, BackButton, IssuesList, Pagination} from './styles';
+import { FaArrowLeft } from "react-icons/fa6";
 import api from '../../services/api';
 
 export default function Repos({match}){
@@ -9,6 +10,7 @@ export default function Repos({match}){
     const [repositorio, setRepositorio] = useState({});
     const [issues, setIssues] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1); 
   
     useEffect(()=> {
       
@@ -37,11 +39,34 @@ export default function Repos({match}){
 
     }, []);
 
+    useEffect(()=> {
+
+        async function loadIssue(){
+            const nomeRepo = decodeURIComponent(inputRepositorio);
+
+            const response = await api.get(`/repos/${nomeRepo}/issues`, {
+                params:{
+                    state: 'open',
+                    page,
+                    per_page: 5,
+                },
+            });
+
+            setIssues(response.data);
+        }
+
+        loadIssue();
+
+    } , [page]);
+
+    function handlePage(action){
+        setPage(action === 'back' ? page - 1 : page + 1);
+    }
     
     if(loading){
         return(
             <Loading>
-                <h1>A carregar dados</h1>
+                <h1>A carregar dados...</h1>
             </Loading>
         )
     }else{
@@ -50,6 +75,9 @@ export default function Repos({match}){
 
         return(
             <Container>
+                <BackButton to="/">
+                    <FaArrowLeft color="#000" size={30}/>
+                </BackButton>
                 <Owner>
                     <img 
                     src={repositorio?.owner.avatar_url} 
@@ -58,6 +86,41 @@ export default function Repos({match}){
                     <h1>{repositorio.name}</h1>
                     <p>{repositorio.description}</p>
                 </Owner>
+
+                <IssuesList>
+                    {issues.map(issue => (
+                        <li key={String(issue.id)}>
+                            <img src={issue.user.avatar_url} alt={issue.user.login}/>
+
+                            <div>
+                                <strong>
+                                    <a href={issue.html_url}>{issue.title}</a>
+                                
+                                    {issue.labels.map(label => (
+                                        <span key={String(label.id)}>{label.name}</span>
+                                    ))}
+
+                                </strong>
+
+                                <p>{issue.user.login}</p>
+
+                            </div>
+                        </li>
+                    ))}
+                </IssuesList>
+
+                <Pagination>
+                    <button 
+                    type="button" 
+                    onClick={()=> handlePage('back')}
+                    disabled={page < 2}>
+                        Anterior
+                    </button>
+                    
+                    <button type="button" onClick={()=> handlePage('next')}>
+                        Seguinte
+                    </button>
+                </Pagination>
             </Container>
         )
     }
